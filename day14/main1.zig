197ba0af3e488d4f6c4588722731b7ffd2fc6efc
Kirl Strezikozin <kirilstrezikozin@gmail.com>
2024-12-20 18:25:51 +0100

    add (day14/20): part 2 solution

--- day14/main1.zig
+++ day14/main1.zig
@@ -23,6 +23,7 @@ const Visited = 'O';
 const MaxUsize = std.math.maxInt(usize);
 const WalkCost = @as(usize, 1);
 const MaxCheat = @as(usize, 20);
+const MinSaving = @as(usize, 100);
 
 const PrintType = enum {
     Unicode,
@@ -79,8 +80,17 @@ fn Map(comptime T: type) type {
     };
 }
 
-const Cheat = std.ArrayList(usize);
-const Cheats = Map(Cheat);
+const Cheats = std.AutoHashMap(usize, usize);
+
+fn SortCtx(comptime T: anytype) type {
+    return struct {
+        const Self = @This();
+        values: []const T,
+        fn asc(self: Self, lhs: usize, rhs: usize) bool {
+            return self.values[lhs] < self.values[rhs];
+        }
+    };
+}
 
 /// Returns a 2d view onto the given file buffer, split by newline characters.
 /// And a byte offset to continue reading from.
@@ -128,32 +138,84 @@ fn read_map(ally: Allocator, b: []u8) !struct { m: Map(u8), s: Location, e: Loca
     return .{ .m = map, .s = s, .e = e };
 }
 
-fn explore_cheat(map: Map(u8), d: usize, cs: Cheats, y: usize, x: usize, dir_i: usize, n: usize) !void {
-    if (n > MaxCheat) return;
+fn ccheat(map: Map(u8), ds: Map(usize), chs: *Cheats, y: usize, x: usize) !void {
+    const cy_l: usize = if (y < MaxCheat) 0 else y - MaxCheat;
+    const cy_r: usize = @min(y + MaxCheat, map.data.len - 1);
 
-    for (0..Directions.len) |i| {
-        // Cannot follow the direction it came from.
-        if ((dir_i < Directions.len) and (i == Directions.len - dir_i - 1)) continue;
+    for (cy_l..cy_r + 1) |cy| {
+        const cells: usize = MaxCheat - (if (y > cy) y - cy else cy - y);
 
-        const ny: usize = @intCast(@as(i64, @intCast(y)) + Directions[i].dy);
-        const nx: usize = @intCast(@as(i64, @intCast(x)) + Directions[i].dx);
+        const cx_l: usize = if (x < cells) 0 else x - cells;
+        const cx_r: usize = @min(x + cells, map.data[y].len - 1);
 
-        if ((nx == 0) or (nx == map.data[ny].len - 1) or
-            (ny == 0) or (ny == map.data.len - 1))
-        {
-            continue;
+        for (cx_l..cx_r + 1) |cx| {
+            if (ds.data[cy][cx] == MaxUsize) continue; // Not a path.
+
+            const mdist: usize = (if (cy > y) cy - y else y - cy) + (if (cx > x) cx - x else x - cx);
+            const nds = ds.data[y][x] + mdist;
+
+            if (ds.data[cy][cx] <= nds) continue; // No cheat advantage.
+
+            const saving: usize = ds.data[cy][cx] - nds;
+            if (saving < MinSaving) continue;
+
+            const entry = try chs.getOrPut(saving);
+            if (entry.found_existing) {
+                entry.value_ptr.* += 1;
+            } else {
+                entry.value_ptr.* = 1;
+            }
         }
-
-        const ncs = d + WalkCost * n;
-        try cs.data[ny][nx].append(ncs);
-
-        try explore_cheat(map, d, cs, ny, nx, i, n + 1);
     }
+
+    return;
 }
 
-fn explore(map: Map(u8), ds: Map(usize), cs: Cheats, y: usize, x: usize, dir_i: usize) !void {
-    try explore_cheat(map, map.data[y][x], cs, y, x, Directions.len, 0);
+fn cheats(ally: Allocator, map: Map(u8), ds: Map(usize)) !usize {
+    var chs = Cheats.init(ally);
+    defer chs.deinit();
 
+    for (1..map.data.len - 1) |y| {
+        for (1..map.data[y].len - 1) |x| {
+            if (ds.data[y][x] != MaxUsize) {
+                try ccheat(map, ds, &chs, y, x);
+            }
+        }
+    }
+
+    const cap: usize = chs.count();
+    var keys = try ally.alloc(usize, cap);
+    defer ally.free(keys);
+    var values = try ally.alloc(usize, cap);
+    defer ally.free(values);
+    var indices = try ally.alloc(usize, cap);
+    defer ally.free(indices);
+
+    var count: usize = 0;
+    var it = chs.iterator();
+    var i: usize = 0;
+    while (it.next()) |entry| {
+        keys[i] = entry.key_ptr.*;
+        values[i] = entry.value_ptr.*;
+        indices[i] = i;
+        i += 1;
+    }
+
+    const ctx = SortCtx(usize){ .values = keys };
+    std.mem.sort(usize, indices, ctx, SortCtx(usize).asc);
+
+    for (indices) |idx| {
+        count += values[idx];
+        std.debug.print(
+            "There are {d} cheats that save {d} picoseconds.\n",
+            .{ values[idx], keys[idx] },
+        );
+    }
+
+    return count;
+}
+
+fn explore(map: Map(u8), ds: Map(usize), y: usize, x: usize, dir_i: usize) void {
     for (0..Directions.len) |i| {
         // Cannot follow the direction it came from.
         if ((dir_i < Directions.len) and (i == Directions.len - dir_i - 1)) continue;
@@ -169,7 +231,7 @@ fn explore(map: Map(u8), ds: Map(usize), cs: Cheats, y: usize, x: usize, dir_i: 
             Empty => {
                 map.data[ny][nx] = Visited;
                 ds.data[ny][nx] = ds.data[y][x] + WalkCost;
-                try explore(map, ds, cs, ny, nx, i);
+                explore(map, ds, ny, nx, i);
             },
             Visited, StartTile, EndTile => {
                 const nds = ds.data[y][x] + WalkCost;
@@ -178,7 +240,7 @@ fn explore(map: Map(u8), ds: Map(usize), cs: Cheats, y: usize, x: usize, dir_i: 
                 if (ds.data[ny][nx] <= nds) continue;
 
                 ds.data[ny][nx] = nds;
-                try explore(map, ds, cs, ny, nx, i);
+                explore(map, ds, ny, nx, i);
             },
             else => unreachable,
         }
@@ -188,8 +250,8 @@ fn explore(map: Map(u8), ds: Map(usize), cs: Cheats, y: usize, x: usize, dir_i: 
 fn process(ally: Allocator, buff: []u8) !usize {
     const input = try read_map(ally, buff);
     const startTile = input.s;
-    var map = input.m;
-    map.print(.Unicode);
+    const map = input.m;
+    // map.print(.Unicode);
 
     if (map.data.len == 0) {
         @panic("Invalid map size");
@@ -200,67 +262,17 @@ fn process(ally: Allocator, buff: []u8) !usize {
     try ds.alloc(ally, map.data.len, map.data[0].len);
     defer ds.free(ally);
 
-    // Cs s a 2d map of costs enabled by cheating.
-    var cs: Cheats = undefined;
-    try cs.alloc(ally, map.data.len, map.data[0].len);
-    defer cs.free(ally);
-
     for (0..map.data.len) |y| {
         @memset(ds.data[y], MaxUsize);
-        for (0..map.data[y].len) |x| {
-            cs.data[y][x] = try Cheat.initCapacity(ally, 10);
-        }
     }
     ds.data[startTile.y][startTile.x] = 0;
 
-    std.debug.print("start tile at {any}\n", .{startTile});
-    try explore(map, ds, cs, startTile.y, startTile.x, Directions.len);
+    explore(map, ds, startTile.y, startTile.x, Directions.len);
 
-    map.print(.Unicode);
-    ds.print(.Numeric);
-    // std.debug.print("\nCheat map:\n", .{});
-    // cs.print(.Numeric);
+    // map.print(.Unicode);
+    // ds.print(.Numeric);
 
-    var savings = std.ArrayList(usize).init(ally);
-    defer savings.deinit();
-
-    // Calculate savings from the pre-calculated map of cheats.
-
-    var count: usize = 0;
-    for (0..map.data.len) |y| {
-        for (0..map.data[y].len) |x| {
-            for (cs.data[y][x].items) |cheat| {
-                const d = ds.data[y][x];
-                if (d <= cheat) continue;
-
-                const saving = d - cheat;
-                if (saving >= 50) {
-                    count += 1;
-                    try savings.append(saving);
-                }
-            }
-        }
-    }
-
-    std.mem.sort(usize, savings.items, {}, comptime std.sort.desc(usize));
-    std.debug.print("\nCheats:\n", .{});
-    var last_saving: usize = 0;
-    var this_savings: usize = 1;
-    for (savings.items) |saving| {
-        if (saving == last_saving) {
-            this_savings += 1;
-            continue;
-        }
-
-        if (last_saving != 0) {
-            std.debug.print("There are {d} cheats that save {d} picoseconds.\n", .{ this_savings, last_saving });
-        }
-        last_saving = saving;
-        this_savings = 1;
-    }
-    std.debug.print("There are {d} cheats that save {d} picoseconds.\n", .{ this_savings, last_saving });
-
-    return count;
+    return try cheats(ally, map, ds);
 }
 
 pub fn main() !void {
