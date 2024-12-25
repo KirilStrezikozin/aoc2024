b77a85e618e36559f5fb5d0808e880b7f6c59107
Kiril Strezikozin <kirilstrezikozin@gmail.com>
2024-12-25 15:55:48 +0100

    cleanup (day17/23): part 1 solution
    Signed-off-by: Kiril Strezikozin <kirilstrezikozin@gmail.com>

--- day17/main0.zig
+++ day17/main0.zig
@@ -3,13 +3,8 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const Endpoint = u16;
-const Connections = std.AutoHashMap(Endpoint, Connection);
+const Connections = std.AutoHashMap(Endpoint, void);
 const Graph = std.AutoHashMap(Endpoint, Connections);
-
-const Connection = enum(u1) {
-    Free,
-    Blocked,
-};
 
 const Trackable = struct {
     me: Endpoint,
@@ -17,7 +12,6 @@ const Trackable = struct {
     target: Endpoint,
 
     depth: usize,
-    pickle: usize,
 };
 
 const MoveBits = @bitSizeOf(u8);
@@ -66,22 +60,11 @@ const Cycles = struct {
     /// If the cycle is already present, returns false.
     /// Otherwise, stores it and returns true.
     fn hash(self: *Self, es: *const [3]Endpoint) !bool {
-        std.debug.print(
-            "Given {s}-{s}-{s},",
-            .{
-                endpointAsSlice(&es[0]),
-                endpointAsSlice(&es[1]),
-                endpointAsSlice(&es[2]),
-            },
-        );
-
         std.sort.block(Endpoint, @constCast(es), {}, std.sort.asc(Endpoint));
 
         const cycle = (@as(Cycle, @intCast(es[2])) << @bitSizeOf(Endpoint) * 2) |
             @as(Cycle, @intCast(es[1])) << @bitSizeOf(Endpoint) |
             @as(Cycle, @intCast(es[0]));
-
-        const cycleh = endpointAsSlice(&es[0]) ++ endpointAsSlice(&es[1]) ++ endpointAsSlice(&es[2]);
 
         const has_t = blk: {
             for (es) |e| {
@@ -89,14 +72,6 @@ const Cycles = struct {
             }
             break :blk false;
         };
-
-        std.debug.print(
-            "hash = {s}, has_t = {}\n",
-            .{
-                cycleh,
-                has_t,
-            },
-        );
 
         return try self.data.fetchPut(cycle, {}) == null and has_t;
     }
@@ -123,18 +98,18 @@ fn read_graph(ally: Allocator, buff: []u8) !struct { Graph, Endpoint } {
         // Assign bi-directional connections.
 
         if (graph.getPtr(from)) |connections| {
-            try connections.put(to, .Free);
+            try connections.put(to, void{});
         } else {
             var connections = Connections.init(ally);
-            try connections.put(to, .Free);
+            try connections.put(to, void{});
             try graph.put(from, connections);
         }
 
         if (graph.getPtr(to)) |connections| {
-            try connections.put(from, .Free);
+            try connections.put(from, void{});
         } else {
             var connections = Connections.init(ally);
-            try connections.put(from, .Free);
+            try connections.put(from, void{});
             try graph.put(to, connections);
         }
     }
@@ -143,55 +118,16 @@ fn read_graph(ally: Allocator, buff: []u8) !struct { Graph, Endpoint } {
 }
 
 fn dfs(graph: *Graph, trackable: Trackable, cycles: *Cycles) !usize {
-    if (trackable.depth == 3) {
-        // if ((trackable.me == trackable.target) and (trackable.pickle < 3)) return 1;
-        return 0;
-    }
+    if (trackable.depth == 3) return 0;
 
     var loops: usize = 0;
     var connections = graph.get(trackable.me).?.iterator();
 
     while (connections.next()) |connection| {
         const neighbour: Endpoint = connection.key_ptr.*;
-        const edge: *Connection = connection.value_ptr;
-
         if (neighbour == trackable.from) continue;
 
-        // std.debug.print(
-        //     "Want to reach {s} from {s}, continue through {s} at depth {d}, but new pickle will be {d}\n",
-        //     .{
-        //         endpointAsSlice(&trackable.target),
-        //         endpointAsSlice(&trackable.me),
-        //         endpointAsSlice(&neighbour),
-        //         trackable.depth,
-        //         trackable.pickle + @intFromEnum(edge.*),
-        //     },
-        // );
-
-        const npickle = trackable.pickle + @intFromEnum(edge.*);
-        if ((neighbour == trackable.target) and (trackable.depth == 2) and (npickle < 3)) {
-            // edge.* = .Blocked;
-
-            // const dedge = graph.get(neighbour).?.getPtr(trackable.me).?;
-            // dedge.* = .Blocked;
-
-            // std.debug.print(
-            //     "\nLoop {s}-{s}-{s}\n\n",
-            //     .{
-            //         endpointAsSlice(&trackable.target),
-            //         endpointAsSlice(&trackable.me),
-            //         endpointAsSlice(&trackable.from),
-            //     },
-            // );
-
-            // std.debug.print(
-            //     "Marking edge {s}-{s} directional\n",
-            //     .{
-            //         endpointAsSlice(&trackable.me),
-            //         endpointAsSlice(&neighbour),
-            //     },
-            // );
-
+        if ((neighbour == trackable.target) and (trackable.depth == 2)) {
             loops += @intFromBool(
                 try cycles.hash(&.{
                     trackable.target,
@@ -210,7 +146,6 @@ fn dfs(graph: *Graph, trackable: Trackable, cycles: *Cycles) !usize {
                 .from = trackable.me,
                 .target = trackable.target,
                 .depth = trackable.depth + 1,
-                .pickle = npickle,
             },
             cycles,
         );
@@ -240,7 +175,6 @@ fn process(ally: Allocator, buff: []u8) !usize {
                 .from = root,
                 .target = root,
                 .depth = 0,
-                .pickle = 0,
             },
             &cycles,
         );
